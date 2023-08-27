5482c11731ce6aeb2527169e47419a22369fe47d
t.nester <t.nester@disoft.dev>
2023-08-27 18:57:56 +0300

    update user model and migration

--- sql_app/models.py
+++ sql_app/models.py
@@ -15,12 +15,12 @@ class User(Base):
     __tablename__ = 'user'
 
     id = Column(Integer, primary_key=True, index=True)
+    telegram_id = Column(Integer, nullable=True, default=None, index=True)
     email = Column(String, unique=True, index=True)
     password = Column(String)
     is_active = Column(Boolean, default=True)
     user_type = Column(Enum(UserTypeEnum), default=UserTypeEnum.CLIENT)
     has_api_access = Column(Boolean, default=False)
-    telegram_id = Column(Integer, nullable=True, default=None)
     first_name = Column(String(35), default='')
     last_name = Column(String(35), nullable=True, default='')
     full_name = Column(String(35), nullable=True, default='')
