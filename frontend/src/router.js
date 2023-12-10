d76f263de2bca067e7dd1fbaaefe747e5a43e0a0
t.nester <t.nester@disoft.dev>
2023-12-10 10:58:48 +0200

    add hello world page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,9 +1,11 @@
+import HelloWorld from "@/components/HelloWorld.vue";
 import ReceiptList from "@/components/ReceiptList.vue";
 import ReceiptDetail from "@/components/ReceiptDetail.vue";
 
 const routes = [
-  { path: "/receipts", name: "receipt-list", component: ReceiptList },
-  { path: "/receipts/:id", name: "receipt-detail", component: ReceiptDetail }
+    { path: "/", name: "home", component: HelloWorld },
+    { path: "/receipts", name: "receipt-list", component: ReceiptList },
+    { path: "/receipts/:id", name: "receipt-detail", component: ReceiptDetail }
 ];
 
 export default routes;
